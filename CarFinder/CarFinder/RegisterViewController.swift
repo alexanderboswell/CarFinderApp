8f5ab9a353429d74e5376e65ca7376470cb4c4b5
Alex Boswell <alexanderboswell@gmail.com>
2017-05-05 18:04:07 -0600

    Add registeration for add

---
+++ CarFinder/CarFinder/RegisterViewController.swift
@@ -0,0 +1,56 @@
+//
+//  RegisterViewController.swift
+//  CarFinder
+//
+//  Created by alexander boswell on 5/5/17.
+//  Copyright © 2017 alexander boswell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func registerAccount(_ sender: UIButton) {
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        FIRAuth.auth()?.createUser(withEmail: email!, password: password!, completion: { (user, error) in
+            if let error = error {
+                if let errCode = FIRAuthErrorCode(rawValue: error._code) {
+                    switch errCode {
+                    case .errorCodeInvalidEmail:
+                        self.showAlert("Enter a valid email.")
+                    case .errorCodeEmailAlreadyInUse:
+                        self.showAlert("Email already in use.")
+                    default:
+                        self.showAlert("Error: \(error.localizedDescription)")
+                    }
+                }
+                return
+            }
+            self.signIn()
+        })
+    }
+    @IBAction func backToLogin(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: {})
+    }
+    
+    func showAlert(_ message: String){
+            let alertController = UIAlertController(title: "CarFinder", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController,animated: true,completion: nil)
+        
+        
+    }
+    func signIn(){
+        
+    }
+
+    
+    
+}
