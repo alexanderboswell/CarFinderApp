e57e9a84b01e5a60135cf67e09ca356305c7c7c0
Alex Boswell <alexanderboswell@gmail.com>
2017-05-16 22:44:33 -0600

    add app icon, set up MapKit and get pins showing

--- CarFinder/CarFinder/MainViewController.swift
+++ CarFinder/CarFinder/MainViewController.swift
@@ -9,16 +9,56 @@
 import Foundation
 import UIKit
 import FirebaseAuth
+import MapKit
 
-
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        mapView.delegate = self
+        // set initial location in st louis
+        let initialLocation = CLLocation(latitude: 38.623283, longitude: -90.190816)
+        centerMapOnLocation(location: initialLocation)
+    }
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+        let pin = Pin(title: "Busch Stadium",
+                              locationName: "Cardinals Baseball Stadium",
+                              coordinate: CLLocationCoordinate2D(latitude: 38.6223399, longitude: -90.192415))
+        
+        mapView.addAnnotation(pin)
+    }
+    // maybe need this ? --------------------------
+    var locationManager = CLLocationManager()
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
+    }
+    // -------------------------------------
 
     @IBAction func signOut(_ sender: UIBarButtonItem) {
         do {
             try FIRAuth.auth()?.signOut()
-         //   performSegue(withIdentifier: "SignOut", sender: nil)
         } catch let error {
             assertionFailure("Error signing out: \(error)")
         }
+        signOut()
     }
+    func signOut() {
+        performSegue(withIdentifier: "signOutSegue", sender: nil)
+    }
+
 }
---
+++ CarFinder/CarFinder/Pin.swift
@@ -0,0 +1,39 @@
+//
+//  Pin.swift
+//  CarFinder
+//
+//  Created by alexander boswell on 5/16/17.
+//  Copyright © 2017 alexander boswell. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import AddressBook
+
+class Pin : NSObject, MKAnnotation {
+    let locationName: String
+    let title: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    init( title: String, locationName: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    var subtitle: String? {
+        return locationName
+    }
+    // annotation callout info button opens this mapItem in Maps app
+    func mapItem() -> MKMapItem {
+        let addressDictionary = [String(kABPersonAddressStreetKey): subtitle]
+        let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: addressDictionary)
+        
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = title
+        
+        return mapItem
+    }
+    
+}
