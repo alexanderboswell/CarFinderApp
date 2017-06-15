a95b3d9e3564e9f57690fb39ed9301b717c5640c
Alex Boswell <alexanderboswell@gmail.com>
2017-06-15 06:33:55 -0600

    refactor FriendRequestViewController

--- CarFinder/CarFinder/FriendRequestViewController.swift
+++ CarFinder/CarFinder/FriendRequestViewController.swift
@@ -18,20 +18,10 @@ class FriendRequestViewController: UIViewController, UITableViewDelegate, UITabl
     
     var friendRequests = [User]()
     
-    // MARK: FireBase Variables
-    var user: FIRUser!
-    
-    var ref: FIRDatabaseReference!
-    
-    private var databaseHandle: FIRDatabaseHandle!
-    
     // MARK: Overriden functions
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // set up Firebase
-        user = FIRAuth.auth()?.currentUser
-        ref = FIRDatabase.database().reference()
         startObservingDataBase()
     }
     @IBAction func close(_ sender: UIBarButtonItem) {
@@ -51,25 +41,25 @@ class FriendRequestViewController: UIViewController, UITableViewDelegate, UITabl
         let id = user.id
         cell.setFunction {
             self.friendRequests.remove(at:indexPath.row)
-            self.acceptRequestToUser(id!)
+            FireBaseDataObject.system.acceptRequestToUser(id!)
             self.tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
             self.tableView.reloadData()
         }
         cell.setFunctionDecline {
             self.friendRequests.remove(at: indexPath.row)
-            self.declineRequestToUser(id!)
+            FireBaseDataObject.system.declineRequestToUser(id!)
             self.tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
             self.tableView.reloadData()
         }
         return cell
     }
     func startObservingDataBase(){
-        let current_user_ref = FIRDatabase.database().reference().child("users").child(user.uid)
+        let current_user_ref = FIRDatabase.database().reference().child("users").child(FireBaseDataObject.system.CURRENT_USER_ID)
         current_user_ref.child("requests").observe(FIRDataEventType.value,with: {(snapshot) in
             self.friendRequests.removeAll()
             for child in snapshot.children.allObjects as! [FIRDataSnapshot] {
                 let id = child.key
-                self.getUser(id, completion: { (user) in
+                FireBaseDataObject.system.getUser(id, completion: { (user) in
                     self.friendRequests.append(user)
                 self.tableView.reloadData()
                 })
@@ -77,31 +67,4 @@ class FriendRequestViewController: UIViewController, UITableViewDelegate, UITabl
             
         })
     }
-    func getUser(_ userID: String, completion: @escaping (User) -> Void) {
-        FIRDatabase.database().reference().child("users").child(userID).observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
-            let email = snapshot.childSnapshot(forPath: "email").value as! String
-            let name = snapshot.childSnapshot(forPath: "name").value as! String
-            let id = snapshot.key
-            completion(User(userEmail: email, userID: id, userName: name))
-        })
-    }
-    func acceptRequestToUser(_ userID: String){
-        CURRENT_USER_REF.child("requests").child(userID).removeValue()
-        CURRENT_USER_REF.child("friends").child(userID).setValue(true)
-        FIRDatabase.database().reference().child("users").child(userID).child("friends").child(CURRENT_USER_ID).setValue(true)
-        FIRDatabase.database().reference().child("users").child(userID).child("requests").child(CURRENT_USER_ID).removeValue()
-    }
-    func declineRequestToUser(_ userID: String){
-        CURRENT_USER_REF.child("requests").child(userID).removeValue()
-    }
-    var CURRENT_USER_ID: String {
-        let id = FIRAuth.auth()?.currentUser!.uid
-        return id!
-    }
-    var CURRENT_USER_REF: FIRDatabaseReference {
-        let id = FIRAuth.auth()?.currentUser!.uid
-        return USER_REF.child("\(id!)")
-    }
-    let USER_REF = FIRDatabase.database().reference().child("users")
-    
 }
